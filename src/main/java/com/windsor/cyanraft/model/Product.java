1975d0248dca18711025ba3d2631c9825cf8f443
windsor <c3fm38@gmail.com>
2023-04-29 16:38:09 +0800

    商品功能 - Refactor category, use Enum

--- src/main/java/com/windsor/cyanraft/model/Product.java
+++ src/main/java/com/windsor/cyanraft/model/Product.java
@@ -1,5 +1,6 @@
 package com.windsor.cyanraft.model;
 
+import com.windsor.cyanraft.constant.ProductCategory;
 import lombok.Data;
 
 import java.util.Date;
@@ -8,7 +9,7 @@ import java.util.Date;
 public class Product {
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
