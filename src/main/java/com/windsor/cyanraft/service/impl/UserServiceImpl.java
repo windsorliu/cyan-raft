a87c41b4915cb9a2657b917423e3c85756c3bf79
windsor <c3fm38@gmail.com>
2023-05-09 13:12:39 +0800

    帳號功能 - 使用MD5加密密碼

--- src/main/java/com/windsor/cyanraft/service/impl/UserServiceImpl.java
+++ src/main/java/com/windsor/cyanraft/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -35,7 +36,11 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        //創建帳號
+        // 使用MD5生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
+        // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
@@ -43,12 +48,17 @@ public class UserServiceImpl implements UserService {
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        // 檢查user是否存在
         if (user == null) {
             log.warn("The email: {} has not been registered yet", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+        // 使用MD5生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // 比較密碼
+        if (user.getPassword().equals(hashedPassword)) {
             return user;
         } else {
             log.warn("email: {} wrong password", userLoginRequest.getEmail());
